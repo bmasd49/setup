46bc0275a33608cff94a3c49cfd7ea258554408e
bmasd49 <60316196+bmasd49@users.noreply.github.com>
2020-09-04 21:02:18 +0900

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -3,11 +3,11 @@
 echo "Hello $USER, you are about to install bmasd49's Linux setup."
 
 read -p "Are you sure that you want to do this? [y/n] " -r
-echo 
+echo 										  #Insert a blank line
 
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 #exit
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 				  #Exit
 fi
 
 echo "Okay, let's go"
@@ -24,7 +24,7 @@ copyDotFiles () {
 }
 
 installChrome () {
-	if ! dpkg-query -l google-chrome-stable >/dev/null 2>&1; then 
+	if ! dpkg-query -l google-chrome-stable >/dev/null 2>&1; then             #If chrome is not installed
 		wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp/
 		apt install /tmp/google-chrome-stable_current_amd64.deb
 	else 
@@ -33,7 +33,7 @@ installChrome () {
 }
 
 installVimPlugins () {
-	if test -f ~/.vim/autoload/plug.vim; then
+	if test -f ~/.vim/autoload/plug.vim; then                                 #If plug.vim is not found
 		curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 		vim +'PlugInstall --sync' +qa
 		python3 ~/.vim/plugged/YouCompleteMe/install.py --clangd-completer
@@ -43,7 +43,7 @@ installVimPlugins () {
 }
 
 installBamboo () {
-	if ! dpkg-query -l ibus-bamboo >/dev/null 2>&1; then 
+	if ! dpkg-query -l ibus-bamboo >/dev/null 2>&1; then 			 #If ibus-bamboo is not installed
 		sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 		sudo apt-get update
 		sudo apt-get install ibus-bamboo
@@ -55,7 +55,7 @@ installBamboo () {
 
 echo
 read -p "Do you want to update all packages and install essential packages (build-essential cmake vim python3-dev texlive-latex-extra)? [y/n] " -r
-echo    # (optional) move to a new line
+echo  
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	installEssentialPackages
@@ -63,7 +63,7 @@ fi
 
 echo
 read -p "Do you want to copy dotfiles (.vimrc, .bashrc, .bash_aliases, .inputrc) [y/n] " -r
-echo    # (optional) move to a new line
+echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	copyDotFiles
@@ -72,7 +72,7 @@ fi
 
 echo
 read -p "Do you want to install Chrome? [y/n] " -r
-echo    # (optional) move to a new line
+echo  
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	installChrome
@@ -80,7 +80,7 @@ fi
 
 echo
 read -p "Do you want to install Vim-Plug and vim's plugins? [y/n] " -r
-echo    # (optional) move to a new line
+echo   
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	installVimPlugins
@@ -88,10 +88,11 @@ fi
 
 echo
 read -p "Do you want to install ibus-bamboo (Vietnamese input method)? [y/n] " -r
-echo    # (optional) move to a new line
+echo  
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	installBamboo
 fi
 
+echo
 echo "All done"
